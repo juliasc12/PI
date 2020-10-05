bdfb088fffac66f46aa04d44e4f187c49f7fa76f
juliasc2 <juzicastro12@gmail.com>
2020-10-05 16:26:00 -0300

    refazendo o ex3 e alterando coisas no brasonic

--- tarefa2/ex3_certo.py
+++ tarefa2/ex3_certo.py
@@ -1,52 +1,36 @@
-import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-
-def showImg(img):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    plt.imshow(img)
-    plt.show()
+import os
 
 def getColor(img,x,y):
     return img.item(y,x,0), img.item(y,x,1),img.item(y,x,2)
-
-def setColor(img, x,y, b, g, r):
-    img.itemset((y,x,0), b)
-    img.itemset((y,x,1), g)
-    img.itemset((y,x,2), r)
-
-def imgAzul(img,altura,largura):
-    for y in range(0,altura):
-        for x in range(0,largura):
-            azul,verde,vermelho = getColor(img,x,y)
-            setColor(img,x,y,azul,0,0)
-
-    cv2.imwrite("LisaAzul.jpg",img)
-
-def imgVerde(img,altura,largura):
-    for y in range(0,altura):
-        for x in range(0,largura):
-            azul,verde,vermelho = getColor(img,x,y)
-            setColor(img,x,y,0,verde,0)
-    cv2.imwrite("LisaVerde.jpg",img)
-
-def imgVermelho(img,altura,largura):
-    for y in range(0,altura):
-        for x in range(0,largura):
-            azul,verde,vermelho = getColor(img,x,y)
-            setColor(img,x,y,0,0,vermelho)
-
-    LisaVermelha = cv2.imwrite("LisaVermelha.jpg",img)
-
-
 
 def main():
     img = cv2.imread("lisa2.jpg")
     altura, largura, canais_de_cores = img.shape
 
-    #Lisa_Azul = imgAzul(img,altura,largura)
-    #Lisa_Verde= imgVerde(img,altura,largura)
-    #Lisa_Vermelho = imgVermelho(img,altura,largura)
-    showImg(img)
+    plt.imshow(img[:,:,0], 'gray')
+    plt.show()
+    plt.imshow(img[:,:,1], 'gray')
+    plt.show()
+    plt.imshow(img[:,:,0], 'gray')
+    plt.show()
+
+    contB=0; contG=0; contR=0
+    for y in range(0,altura):
+        for x in range(0,largura):
+            azul,verde,vermelho = getColor(img,x,y)
+            contB = azul+contB
+            contG = verde+contG
+            contR = vermelho+contR
+
+    #img de dimensoes 436x512 = 223.232
+    TotalPixel = 223332
+    MediaB = (contB/TotalPixel)
+    MediaG = (contG/TotalPixel)
+    MediaR = (contR/TotalPixel)
+
+    print("Media Azul: "+str(MediaB)+"/ Media Verde: "+str(MediaG)+"Media Vermelha: "+str(MediaR))
+
 
 main()
--- tarefa2/ex3_errado.py
+++ tarefa2/ex3_errado.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-import os
 
 def showImg(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -16,16 +15,38 @@ def setColor(img, x,y, b, g, r):
     img.itemset((y,x,1), g)
     img.itemset((y,x,2), r)
 
-def main():
-    img = cv2.imread("sonic.jpg")
-    altura, largura, canais_de_cores = img.shape
-
+def imgAzul(img,altura,largura):
     for y in range(0,altura):
         for x in range(0,largura):
             azul,verde,vermelho = getColor(img,x,y)
-            setColor(img,x,y,verde,azul,vermelho)
+            setColor(img,x,y,azul,0,0)
 
+    cv2.imwrite("LisaAzul.jpg",img)
+
+def imgVerde(img,altura,largura):
+    for y in range(0,altura):
+        for x in range(0,largura):
+            azul,verde,vermelho = getColor(img,x,y)
+            setColor(img,x,y,0,verde,0)
+    cv2.imwrite("LisaVerde.jpg",img)
+
+def imgVermelho(img,altura,largura):
+    for y in range(0,altura):
+        for x in range(0,largura):
+            azul,verde,vermelho = getColor(img,x,y)
+            setColor(img,x,y,0,0,vermelho)
+
+    LisaVermelha = cv2.imwrite("LisaVermelha.jpg",img)
+
+
+
+def main():
+    img = cv2.imread("lisa2.jpg")
+    altura, largura, canais_de_cores = img.shape
+
+    #Lisa_Azul = imgAzul(img,altura,largura)
+    #Lisa_Verde= imgVerde(img,altura,largura)
+    #Lisa_Vermelho = imgVermelho(img,altura,largura)
     showImg(img)
-    cv2.imwrite("Brasonic.jpg",img)
 
 main()
